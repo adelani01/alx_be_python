7c9268f1558c8d91851da8bc8856590f3170ecd6
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-05-26 19:52:22 +0100

    area modify

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,4 +1,14 @@
+
+#length of the rectangle
 length = 10 
+
+#width of the rectangle
 width = 5
-area =  length * width
+
+#Formula for area of the rectangle
+Area =  length * width
+
+# Area of the rectangle
+area = Area
+
 print(f"The area of the rectangle is: {area}")
