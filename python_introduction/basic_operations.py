c15f98f1633ae9e901f575c06bf9ea5e8851dc84
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-05-26 19:09:49 +0100

    Basic Arithemetic Exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,16 @@
 
+number1 = 10
+number2 = 5
+
+# Calculating the sum of two numbers
+sum = number1 + number2
+
+# Calculating the difference between two numbers
+difference = number1  - number2
+
+# Calculating the difference between two numbers
+product = number1 * number2
+
+print (f"Addition of {number1} and {number2} is {sum}")
+print (f" Subtraction of {number1} and {number2} is {difference}")
+print (f" Multiplication of {number1} and {number2} is {product}")
