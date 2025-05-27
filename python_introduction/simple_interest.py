ba66b90f4641c0821b01ad9fa005f72cd15dfd51
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-05-27 18:33:12 +0100

    Simple interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+# The principal amount
+principal = 1000
+
+# annual interest rate
+rate = 0.05
+
+# Time the money is invested in years
+time = 3
+
+# Formula for simple interest
+Interest =  principal * rate * time
+
+# print simple interest
+print(f"The simple interest is: {Interest}")
