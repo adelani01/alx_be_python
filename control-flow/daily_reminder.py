6e8d93bc9575e6477842220942bf0fc86f0b75c8
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-05-31 07:47:33 +0100

    mend
    personal daily reminder using Match case

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,7 +4,7 @@
 
 #Prompt for a Single Task:
 
-Task = input("Enter your task :")
+Task = input("Enter your task:")
 Priority = input("Priority (high/medium/low):")
 Time_Bound = input("Is it time-bound? (yes/no):")
 
