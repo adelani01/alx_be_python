38847c3c4bad6c8da37bc992b4acbe9f045412be
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-05-31 07:43:18 +0100

    personal daily reminder using Match case

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+# Personal Daily Reminder
+
+# Objective: Create a simplified Python script that uses conditional statements, Match Case, and loops to remind the user about a single, priority task for the day based on time sensitivity.
+
+#Prompt for a Single Task:
+
+task = input("Enter your task :")
+priority = input("Priority (high/medium/low):")
+time_bound = input("Is it time-bound? (yes/no):")
+
+match time_bound:
+    case "yes":
+        print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!" )
+
+    case "no":
+     print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time." )
+
+
+
