df14524aa1fb4cb2e1007f565ea5eac195689633
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-05-31 08:01:13 +0100

    mend
    personal daily reminder using Match case

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,12 +8,30 @@ Task = input("Enter your task:")
 Priority = input("Priority (high/medium/low):")
 Time_Bound = input("Is it time-bound? (yes/no):")
 
-match Time_Bound:
-    case "yes":
-        print(f"Reminder: '{Task}' is a {Priority} priority task that requires immediate attention today!" )
+match Priority:
+    case "high":
+        if Time_Bound == "yes":
+            print(f"Reminder: '{Task}' is a {Priority} priority task that requires immediate attention today!" )
+        
+        else:
+             print(f"Note: '{Task}' is a {Priority} priority task. Consider completing it when you have free time." )
 
-    case "no":
-     print(f"Note: '{Task}' is a {Priority} priority task. Consider completing it when you have free time." )
+
+    case "medium":
+        if Time_Bound == "yes":
+            print(f"Reminder: '{Task}' is a {Priority} priority task that requires immediate attention today!" )
+
+        else:
+             print(f"Note: '{Task}' is a {Priority} priority task. Consider completing it when you have free time." )
+
+    case "low":
+        if Time_Bound == "yes":
+            print(f"Reminder: '{Task}' is a {Priority} priority task that requires immediate attention today!" )
+
+        else:
+             print(f"Note: '{Task}' is a {Priority} priority task. Consider completing it when you have free time." )
+
+    
 
 
 
