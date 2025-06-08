fa6476718426fa76cfc977420babaf69f657b489
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-06-08 17:10:32 +0100

    performing operation using function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,8 @@
 def perform_operation(num1, num2, operation):
-    
+
     if operation == 'add':
         return num1 + num2
-    elif operation == 'subract':
+    elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2 
