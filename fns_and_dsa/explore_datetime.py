fd31fc4305bc01f8e726689378563ae15d11cbf3
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-06-09 00:27:22 +0100

    calculating future-date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,15 @@
+#dateime module
 from datetime import date, datetime, timedelta
 
+
+# defining current date
 def display_current_datetime():
     current_date = datetime.now()
-    print("Current date and time: ", current_date)
-    return current_date
+    formatted_date= current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time: ", formatted_date)
+    return formatted_date
 
-
+# calculating future date
 def calculate_future_date():
     today = date.today()
     number_of_days = int(input("Enter the number of days to add to the current date: "))
