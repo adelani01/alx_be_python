b423a303c03f7a4347f528a18218773bd1530b66
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-06-08 15:49:36 +0100

    performing operation using function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,10 +6,10 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         result = num1 * num2
     elif operation == 'divide':
-       if num2 == 0 :
+         if num2 == 0 :
              return "Cannot divide by zero"
-       else:
-            result = num1 / num2
+         else:
+             result = num1 / num2
             
     
     
