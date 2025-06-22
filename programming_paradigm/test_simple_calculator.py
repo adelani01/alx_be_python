d95e2a685b69bf556f5f5e2f3e9b38b5bfbf20ec
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-06-22 19:39:33 +0100

    trying out unit test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,35 +9,35 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_addition(self):
         """Test the addition method."""
-        calc = SimpleCalculator()
-        self.assertEqual(calc.add(10,5),15)
-        self.assertEqual(calc.add(-10,5),-5)
-        self.assertEqual(calc.add(-1,-1),-2)
-        self.assertEqual(calc.add(-1,1),0)
+        #calc = SimpleCalculator()
+        self.assertEqual(self.calc.add(10,5),15)
+        self.assertEqual(self.calc.add(-10,5),-5)
+        self.assertEqual(self.calc.add(-1,-1),-2)
+        self.assertEqual(self.calc.add(-1,1),0)
 
     def test_subtract(self):
         """Test the addition method."""
-        calc = SimpleCalculator()
-        self.assertEqual(calc.subtract(10,5),5)
-        self.assertEqual(calc.subtract(2,5),-3)
-        self.assertEqual(calc.subtract(-1,-1),0)
-        self.assertEqual(calc.subtract(-1,4),-5)
+        #calc = SimpleCalculator()
+        self.assertEqual(self.calc.subtract(10,5),5)
+        self.assertEqual(self.calc.subtract(2,5),-3)
+        self.assertEqual(self.calc.subtract(-1,-1),0)
+        self.assertEqual(self.calc.subtract(-1,4),-5)
 
     def test_multiply(self):
         """Test the addition method."""
-        calc = SimpleCalculator()
-        self.assertEqual(calc.multiply(10,0),0)
-        self.assertEqual(calc.multiply(-10,5),-50)
-        self.assertEqual(calc.multiply(-5,-1),5)
-        self.assertEqual(calc.multiply(5,-6),-30)
+        #calc = SimpleCalculator()
+        self.assertEqual(self.calc.multiply(10,0),0)
+        self.assertEqual(self.calc.multiply(-10,5),-50)
+        self.assertEqual(self.calc.multiply(-5,-1),5)
+        self.assertEqual(self.calc.multiply(5,-6),-30)
     
     def test_divide(self):
         """Test the addition method."""
-        calc = SimpleCalculator()
-        self.assertEqual(calc.divide(10,5),2)
-        self.assertEqual(calc.divide(-10,5),-2)
-        self.assertEqual(calc.divide(-10,-10),1)
-        self.assertEqual(calc.divide(20,100),0.2)
+       #calc = SimpleCalculator()
+        self.assertEqual(self.calc.divide(10,5),2)
+        self.assertEqual(self.calc.divide(-10,5),-2)
+        self.assertEqual(self.calc.divide(-10,-10),1)
+        self.assertEqual(self.calc.divide(20,100),0.2)
 
 
 
