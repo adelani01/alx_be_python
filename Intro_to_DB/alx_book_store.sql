45138b9d42772ecd7bbc06bf91cde26563adf6d8
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 19:16:01 +0100

    introduction to database

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,44 +1,51 @@
---Create Database
+-- Create Database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 
 USE alx_book_store;
 
---Stores information about authors.
+-- Stores information about authors.
 CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT Primary Key,
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 
---Stores information about books available in the bookstore.
+-- tores information about books available in the bookstore.
 CREATE TABLE IF NOT EXISTS BOOKS(
-    book_id INT AUTO_INCREMENT Primary Key,
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
-    Foreign Key(author_id) REFERENCES Authors (author_id),
+    author_id INT,
     price DOUBLE,
-    publication_date DATE
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+   
 );
 
---Stores information about customers.
+-- Stores information about customers.
 CREATE TABLE IF NOT EXISTS Customers(
-    customer_id  INT AUTO_INCREMENT Primary Key,
+    customer_id  INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
---Stores information about orders placed by customers.
+-- Stores information about orders placed by customers.
 CREATE TABLE IF NOT EXISTS Orders(
-    order_id  INT AUTO_INCREMENT Primary Key,
-    Foreign Key(customer_id) REFERENCES Customers(customer_id),
-    order_date DATE NOT NULL
+    order_id  INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id  INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    
 );
 
---Stores information about the books included in each order.
+-- Stores information about the books included in each order.
 CREATE TABLE IF NOT EXISTS Order_Details(
-    orderdetailid INT  AUTO_INCREMENT Primary Key,
-    Foreign Key(order_id) REFERENCES Orders (order_id),
-    Foreign Key(book_id) REFERENCES BOOKS (book_id),
-    quantity DOUBLE NOT NULL
+    orderdetailid INT  AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    order_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES BOOKS(book_id)
+    
 );
