d1639a70b4848e6bc22739af031530e5d99fb8ae
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 19:30:42 +0100

    Create alx_book_store.sql with full bookstore schema

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- Stores information about authors.
-CREATE TABLE IF NOT EXISTS Authors (
+CREATE TABLE IF NOT EXISTS AUTHORS (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
@@ -18,12 +18,12 @@ CREATE TABLE IF NOT EXISTS BOOKS(
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+    FOREIGN KEY(author_id) REFERENCES AUTHORS (author_id)
    
 );
 
 -- Stores information about customers.
-CREATE TABLE IF NOT EXISTS Customers(
+CREATE TABLE IF NOT EXISTS CUSTOMERS(
     customer_id  INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -31,21 +31,20 @@ CREATE TABLE IF NOT EXISTS Customers(
 );
 
 -- Stores information about orders placed by customers.
-CREATE TABLE IF NOT EXISTS Orders(
+CREATE TABLE IF NOT EXISTS ORDERS(
     order_id  INT AUTO_INCREMENT PRIMARY KEY,
     customer_id  INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY(customer_id) REFERENCES CUSTOMERS(customer_id)
     
 );
 
 -- Stores information about the books included in each order.
-CREATE TABLE IF NOT EXISTS Order_Details(
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS(
     orderdetailid INT  AUTO_INCREMENT PRIMARY KEY,
     book_id INT,
     order_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(order_id) REFERENCES ORDERS(order_id),
     FOREIGN KEY(book_id) REFERENCES BOOKS(book_id)
-    
 );
